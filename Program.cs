80e4c17c8de5cf0bcd73056dae24f8377f4a06af
gabovalmon-3 <gabovalmon@icloud.com>
2025-05-17 11:33:38 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Entities;
+using System.Data.Common;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        //Instancia del gato
+
+        var myCat = new Cat() { Id = 1, Name= "test" };
+
+        Console.WriteLine("Id del gato: " + myCat.Id);
+
+        Console.WriteLine("Nombre del gato: " + myCat.Name);
+
+        Console.WriteLine("Ingrese una descripcion para el gato");
+
+        myCat.Description = Console.ReadLine();
+
+
+
+    }
+}
