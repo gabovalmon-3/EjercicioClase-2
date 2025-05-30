4d410494653c36bd4d061c5ad12a4ea507bce7a6
gabovalmon-3 <gabovalmon@icloud.com>
2025-05-29 19:10:29 -0600

    no se subieron en el primer commit

---
+++ Entities/Cats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{ 
+    public class Cat
+{
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime Birth { get; set; }
+
+    public Cat() { }
+
+
+
+    }
+}
